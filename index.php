8825e947eeada846e7595a9fa19736822f3bec74
Amalec Harian Adrove Mejías <amalec.hack@gmail.com>
2014-06-05 19:48:48 -0400

    Esquema principal para trabajar...
    Nada importante... aún.

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">>
+<head>
+<meta charset="utf-8">
+<title>Variables</title>
+</head>
+<body>
 <?php
-printf('Hola mundo');
+printf('<h1>Hola mundo, ésto es un trabajo de variables múltiples o arrays...</h1> <br>');
 ?>
+</body>
+</html>
