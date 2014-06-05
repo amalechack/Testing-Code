52122e49c2a32abf75413c37eebbcedb6e3d90f1
Amalec Harian Adrove Mejías <amalec.hack@gmail.com>
2014-06-05 19:53:45 -0400

    Primeros cambios
    :D algo productivo :P

--- index.php
+++ index.php
@@ -7,6 +7,14 @@
 <body>
 <?php
 printf('<h1>Hola mundo, ésto es un trabajo de variables múltiples o arrays...</h1> <br>');
+
+//empecemos con lo básico...
+
+	//variables...
+	$variable[1] = 'hola';
+	$variable[2] = 'mundo';
+
+	echo $variable[1].' '.$variable[2];
 ?>
 </body>
 </html>
