5db15847454374a82f295acba717a79fc359ac22
Akaicode <sk21patrick@yahoo.jp>
2014-06-05 20:07:37 -0400

    corrección error 2 de 100000000000000000000
    agregue un espacio

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ printf('<h1>Hola mundo, ésto es un trabajo de variables múltiples o arrays...<
 	$variable[2] = 'mundo';
 	$variable[3] = 'maldito';
 
-	echo $variable[1].' '.$variable[2].$variable[3];
+	echo $variable[1].' '.$variable[2].' '.$variable[3];
 ?>
 </body>
 </html>
